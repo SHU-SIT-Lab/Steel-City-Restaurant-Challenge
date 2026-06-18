a9a7e1e9d5384543055038c4afd16d0eb2ddf20f
Samiulhaq Chardiwall <samiulhaq513@gmail.com>
2026-06-18 14:56:18 +0100

    ros2 jazzy docker update

---
+++ configs/turtlebot_setup.bash
@@ -0,0 +1,7 @@
+source /opt/ros/jazzy/setup.bash
+
+export ROS_DOMAIN_ID=0
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+
+export ROS_SUPER_CLIENT=True
+export ROS_DISCOVERY_SERVER="192.168.1.150:11811;"
